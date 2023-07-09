e486f0c8ad85fe216c97f4e6007c733c4248cc33
Bruno Paulon <79466541+brunopaulon2021@users.noreply.github.com>
2023-07-09 17:35:18 +0000

    update contact-list.component.scss

--- src/app/contact.model.ts
+++
@@ -1,8 +0,0 @@
-export interface Contact {
-    id: number;
-    name: string;
-    phoneNumber: string;
-    email: string;
-    address: string;
-    // Outras propriedades que você deseja adicionar
-  }
