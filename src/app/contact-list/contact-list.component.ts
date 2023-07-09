e99cd940e84d96879a4d960f20661fd99acbb2f9
Bruno Paulon <79466541+brunopaulon2021@users.noreply.github.com>
2023-07-09 17:09:07 +0000

    update contact-list.component.html, contact-list.component.scss and contact-list.component.ts

--- src/app/contact-list/contact-list.component.ts
+++ src/app/contact-list/contact-list.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Contact } from '../contact.model';
 
 @Component({
   selector: 'app-contact-list',
   templateUrl: './contact-list.component.html',
   styleUrls: ['./contact-list.component.scss']
 })
-export class ContactListComponent {
+export class ContactListComponent implements OnInit {
+  contacts: Contact[] = [
+    { id: 1, name: 'John Doe', phoneNumber: '1234567890', email: 'john@example.com', address: '123 Main St' },
+    { id: 2, name: 'Jane Smith', phoneNumber: '9876543210', email: 'jane@example.com', address: '456 Elm St' }
+  ];
 
+  constructor() { }
+
+  ngOnInit() {
+  }
 }
---
+++ src/app/contact.model.ts
@@ -0,0 +1,8 @@
+export interface Contact {
+    id: number;
+    name: string;
+    phoneNumber: string;
+    email: string;
+    address: string;
+    // Outras propriedades que você deseja adicionar
+  }
